2f6222e80510bdc66c796cf1119c65ac4b00a19d
denisegoliveira <goliveira.denise@gmail.com>
2021-12-09 17:25:05 -0300

    estruturando js

--- passinho.js
+++ passinho.js
@@ -1,13 +1,30 @@
-var selecao = document.querySelector('#selecao')
-var base = document.querySelector('#divBase')
-var cifra = document.querySelector('#divCifra')
+var botao = document.getElementById('enviar')
+var texto = document.getElementById('texto')
+var numero = document.getElementById('numero')
+var codigo = document.querySelectorAll('.codigo')
+var mensagem = document.getElementById('mensagem')
 
-selecao.addEventListener('change', function() {
-    if(selecao.value == 'base') {
-        base.style.display = 'block'
-        cifra.style.display = 'none'
-    } else {
-        cifra.style.display = 'block'
-        base.style.display = 'none'
+botao.addEventListener('click', () => {
+    var mensagemCodificada = texto.value.split('')
+    var valor = +numero.value
+    if(codigo[0].checked) {
+        mensagem.innerText = cesar(mensagemCodificada, valor)
+    }else {
+        mensagem.innerText = decode(mensagemCodificada, valor)
     }
 })
+
+function cesar(arr, key) {
+    return arr.map((novoArr) => {
+        let char = novoArr.charCodeAt()
+        if(char >= 65 && char <= 90) {
+            return String.fromCharCode(((char -65 + key)%26)+65)
+        }else if (char >= 97 && char <= 122) {
+            return String.fromCharCode(((char -97 + key)%26)+97)
+        } else {
+            return novoArr
+        }  
+        }) .join('')
+    }
+    function decode(arr, key) {
+}
